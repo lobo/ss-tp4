e347175c8eebfedf129ff55d396a27df3cfa42d0
Daniel Lobo <dlobo@itba.edu.ar>
2018-04-06 09:28:45 -0300

    add some format to main

--- src/main/java/ar/edu/itba/ss/tp4/Main.java
+++ src/main/java/ar/edu/itba/ss/tp4/Main.java
@@ -1,7 +1,12 @@
 
 	package ar.edu.itba.ss.tp4;
 
-		/**
+	import java.io.IOException;
+	
+	import com.fasterxml.jackson.core.JsonParseException;
+	import com.fasterxml.jackson.databind.JsonMappingException;
+
+/**
 		* <p>Punto de entrada principal de la simulación. Se encarga de
 		* configurar los parámetros de operación y de desplegar el
 		* sistema requerido.</p>
@@ -9,8 +14,62 @@
 
 	public final class Main {
 
-		public static void main(final String [] arguments) {
-
-			System.out.println("A time-driven simulation...");
+		private static final String HELP_TEXT = "Brownian Motion.\n" +
+				"Possible modes: \n" + 
+				"generate\n" +
+				"simulate\n";
+	
+		enum EXIT_CODE {
+			NO_ARGS(-1), 
+			BAD_N_ARGUMENTS(-2),
+			BAD_ARGUMENT(-3);
+			
+			private final int code;
+		
+			EXIT_CODE(final int code) {
+				this.code = code;
+			}
+		
+			public int getCode() {
+				return code;
+			}
+		}
+		
+		private static void exit(final EXIT_CODE exitCode) {
+			System.exit(exitCode.getCode());
+		}
+	
+		public static void main(final String [] arguments) throws JsonParseException, JsonMappingException, IOException {
+			
+			if (arguments.length == 0) {
+				System.out.println("[FAIL] - No arguments passed. Try 'help' for more information.");
+				exit(EXIT_CODE.NO_ARGS);
+			} else if (arguments.length != 1) {
+				System.out.println("[FAIL] - Wrong number of arguments. Try 'help' for more information.");
+				exit(EXIT_CODE.BAD_N_ARGUMENTS);
+			}
+					
+			if (arguments[0].equals("help")) {
+				System.out.println(HELP_TEXT);
+			} else {
+				//final long start = System.nanoTime();
+				
+				switch (arguments[0]) {
+					case "help":
+						System.out.println(HELP_TEXT);
+						break;
+					case "generate":
+						//generateMode();
+						break;
+					case "simulate":
+						//simulateMode();
+						break;
+					default:
+						System.out.println("[FAIL] - Invalid argument. Try 'help' for more information.");
+						exit(EXIT_CODE.BAD_ARGUMENT);
+						break;
+				}
+			}
+			
 		}
 	}
