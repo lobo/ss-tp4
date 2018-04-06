45a63302e7b0a3f9bad7c705c449157de5703226
Agustín <agustin.golmar@gmail.com>
2018-04-06 17:18:09 -0300

    Main stages.

--- src/main/java/ar/edu/itba/ss/tp4/Main.java
+++ src/main/java/ar/edu/itba/ss/tp4/Main.java
@@ -2,11 +2,11 @@
 	package ar.edu.itba.ss.tp4;
 
 	import java.io.IOException;
-	
+
 	import com.fasterxml.jackson.core.JsonParseException;
 	import com.fasterxml.jackson.databind.JsonMappingException;
 
-/**
+		/**
 		* <p>Punto de entrada principal de la simulación. Se encarga de
 		* configurar los parámetros de operación y de desplegar el
 		* sistema requerido.</p>
@@ -14,33 +14,17 @@
 
 	public final class Main {
 
-		private static final String HELP_TEXT = "Brownian Motion.\n" +
-				"Possible modes: \n" + 
-				"generate\n" +
-				"simulate\n";
-	
-		enum EXIT_CODE {
-			NO_ARGS(-1), 
-			BAD_N_ARGUMENTS(-2),
-			BAD_ARGUMENT(-3);
-			
-			private final int code;
-		
-			EXIT_CODE(final int code) {
-				this.code = code;
-			}
-		
-			public int getCode() {
-				return code;
-			}
+		protected static void simulateMode() {
+			System.out.println("A time-driven simulation...");
 		}
-		
-		private static void exit(final EXIT_CODE exitCode) {
-			System.exit(exitCode.getCode());
+
+		protected static void animateMode() {
+			System.out.println("A time-driven animation...");
 		}
-	
-		public static void main(final String [] arguments) throws JsonParseException, JsonMappingException, IOException {
-			
+
+		public static void main(final String [] arguments)
+				throws JsonParseException, JsonMappingException, IOException {
+
 			if (arguments.length == 0) {
 				System.out.println("[FAIL] - No arguments passed. Try 'help' for more information.");
 				exit(EXIT_CODE.NO_ARGS);
@@ -48,28 +32,44 @@
 				System.out.println("[FAIL] - Wrong number of arguments. Try 'help' for more information.");
 				exit(EXIT_CODE.BAD_N_ARGUMENTS);
 			}
-					
-			if (arguments[0].equals("help")) {
-				System.out.println(HELP_TEXT);
-			} else {
-				//final long start = System.nanoTime();
-				
-				switch (arguments[0]) {
-					case "help":
-						System.out.println(HELP_TEXT);
-						break;
-					case "generate":
-						//generateMode();
-						break;
-					case "simulate":
-						//simulateMode();
-						break;
-					default:
-						System.out.println("[FAIL] - Invalid argument. Try 'help' for more information.");
-						exit(EXIT_CODE.BAD_ARGUMENT);
-						break;
-				}
+			switch (arguments[0]) {
+				case "help":
+					System.out.println("Time-Driven Simulation.\n" +
+							"\tPossible modes: \n" +
+							"\t\tsimulate\n" +
+							"\t\tanimate\n");
+					break;
+				case "simulate":
+					simulateMode();
+					break;
+				case "animate":
+					animateMode();
+					break;
+				default:
+					System.out.println("[FAIL] - Invalid argument. Try 'help' for more information.");
+					exit(EXIT_CODE.BAD_ARGUMENT);
+					break;
 			}
-			
+		}
+
+		protected enum EXIT_CODE {
+
+			NO_ARGS(-1), 
+			BAD_N_ARGUMENTS(-2),
+			BAD_ARGUMENT(-3);
+
+			protected final int code;
+
+			EXIT_CODE(final int code) {
+				this.code = code;
+			}
+
+			public int getCode() {
+				return code;
+			}
+		}
+
+		protected static void exit(final EXIT_CODE exitCode) {
+			System.exit(exitCode.getCode());
 		}
 	}
