ac1e29d9ff952f6ca3508db771af6f7f15f56ce1
Daniel Lobo <dlobo@itba.edu.ar>
2018-04-17 11:53:25 -0300

    change sizes

--- src/main/java/ar/edu/itba/ss/tp4/Main.java
+++ src/main/java/ar/edu/itba/ss/tp4/Main.java
@@ -93,6 +93,8 @@
 			configurator.load();
 			final Configuration config = configurator.getConfiguration();
 			
+			final int N = 6;
+			
 			final String filepath = config.getOutput(); // where I read the simulate file
 			Input input = new Input(filepath);
 			
@@ -102,10 +104,10 @@
 			OutputAnimatedFile output = OutputAnimatedFile.getInstance(filepath + ".xyz");
 			double time = 0.0;
 			
-			for (int i = 0; i < allParticles.size() / 10; i++) {
+			for (int i = 0; i < allParticles.size() / N; i++) {
 				List<MassiveParticle> tenParticles = new ArrayList<>();
-				for (int j = 0; j < 10; j++) {
-					tenParticles.add(allParticles.get(i * 10 + j));
+				for (int j = 0; j < N; j++) {
+					tenParticles.add(allParticles.get(i * N + j));
 				}
 				output.write(tenParticles, time);
 				time += deltat;
@@ -163,20 +165,20 @@
 			final Double voyagerMass = 751.00; //kg
 			
 			// Siempre primero!
-			particles.add(new MassiveParticle(voyagerX, voyagerY, 3.7, voyagerVx, voyagerVy, voyagerMass));	// Voyager
-			particles.add(new MassiveParticle(0, 0, 6.955 * Math.pow(10, 5) * 1000, 0, 0, 1.988544 * Math.pow(10, 30))); // Sun
+			particles.add(new MassiveParticle(voyagerX, voyagerY, 1000 * 1000 * 1000, voyagerVx, voyagerVy, voyagerMass));	// Voyager
+			particles.add(new MassiveParticle(0, 0, 6.955E+8 * 100, 0, 0, 1.988544 * Math.pow(10, 30))); // Sun
 			
-			particles.add(new MassiveParticle(4.934575904901209E+10, -3.332189926157666E+10, 2440 * 1000, 1.768273057783463E+04, 4.262789135023012E+04, 3.302 * Math.pow(10, 26)));  // Mercury
-			particles.add(new MassiveParticle(3.343420365750898E+10, 1.025157427442533E+11, 6051.8 * 1000, -3.341288489965073E+04, 1.069158573237753E+04, 48.685 * Math.pow(10, 23))); 	// Venus
+			particles.add(new MassiveParticle(4.934575904901209E+10, -3.332189926157666E+10, 2440E+03 * 1000, 1.768273057783463E+04, 4.262789135023012E+04, 3.302 * Math.pow(10, 26)));  // Mercury
+			particles.add(new MassiveParticle(3.343420365750898E+10, 1.025157427442533E+11, 6051.8E+03 * 1000, -3.341288489965073E+04, 1.069158573237753E+04, 48.685 * Math.pow(10, 23))); 	// Venus
 
-			particles.add(new MassiveParticle(1.407128872765628E+11, -5.468366774055181E+10, earthRadius, 1.030842712511233E+04, 2.764345679083411E+04, 5.97219 * Math.pow(10, 24)));     // Earth
-			particles.add(new MassiveParticle(1.395051878512483E+11, 1.708511937435906E+11, 3389 * 1000, -1.784109781473363E+04, 1.738371048843349E+04, 6.4185 * Math.pow(10, 23)));     // Mars
+			particles.add(new MassiveParticle(1.407128872765628E+11, -5.468366774055181E+10, earthRadius * 1000, 1.030842712511233E+04, 2.764345679083411E+04, 5.97219 * Math.pow(10, 24)));     // Earth
+			particles.add(new MassiveParticle(1.395051878512483E+11, 1.708511937435906E+11, 3389E+03 * 1000, -1.784109781473363E+04, 1.738371048843349E+04, 6.4185 * Math.pow(10, 23)));     // Mars
 			
-			particles.add(new MassiveParticle(1.103684896887148E+11, 7.543016811214004E+11, 71492 * 1000, -1.309352942244424E+04, 2.503650206820343E+03, 1898.13 * Math.pow(10, 24)));     // Jupiter
-			particles.add(new MassiveParticle(-1.073328635371196E+12, 8.571007177286015E+11, 60268 * 1000, -6.562187951822629E+03, -7.574470327647103E+03, 5.68319 * Math.pow(10, 26)));     // Saturn
+			//particles.add(new MassiveParticle(1.103684896887148E+11, 7.543016811214004E+11, 71492E+03 * 1000, -1.309352942244424E+04, 2.503650206820343E+03, 1898.13 * Math.pow(10, 24)));     // Jupiter
+			//particles.add(new MassiveParticle(-1.073328635371196E+12, 8.571007177286015E+11, 60268E+03 * 1000, -6.562187951822629E+03, -7.574470327647103E+03, 5.68319 * Math.pow(10, 26)));     // Saturn
 			
-			particles.add(new MassiveParticle(-2.078779085304928E+12, -1.847099660309472E+12, 25559 * 1000, 4.460125299069235E+03, -5.407811643968174E+03, 86.8103 * Math.pow(10, 24)));     // Uranus
-			particles.add(new MassiveParticle(-1.126762331215444E+12, -4.387210922478625E+12, 24766 * 1000, 5.215623857879738E+03, -1.321799332943101E+03, 102.41 * Math.pow(10, 24)));     // Neptune
+			//particles.add(new MassiveParticle(-2.078779085304928E+12, -1.847099660309472E+12, 25559 * 1000, 4.460125299069235E+03, -5.407811643968174E+03, 86.8103 * Math.pow(10, 24)));     // Uranus
+			//particles.add(new MassiveParticle(-1.126762331215444E+12, -4.387210922478625E+12, 24766 * 1000, 5.215623857879738E+03, -1.321799332943101E+03, 102.41 * Math.pow(10, 24)));     // Neptune
 			
 			return particles;
 		}
