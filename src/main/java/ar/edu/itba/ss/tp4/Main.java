f12192c4cdebb697d493fcddc74e206f387feab1
Agustín <agustin.golmar@gmail.com>
2018-04-20 18:43:38 -0300

    Me hinché.

--- src/main/java/ar/edu/itba/ss/tp4/Main.java
+++ src/main/java/ar/edu/itba/ss/tp4/Main.java
@@ -152,43 +152,37 @@
 
 			// Earth properties:
 			final double Re = 6371000.0;
-			final Vector earth = Vector.of(1.439499338063904E+11, -4.501039010586976E+10);
-			final Vector Ve = Vector.of(8.415311788888911E+3, 2.831597743867901E+4);
+			final double earthMass = 5.97219E+24;
+			final Vector earth = Vector.of(1.443357209417353E+11, -4.369009909023139E+10);
+			final Vector Ve = Vector.of(8.156143469410742E+3, 2.839717157487163E+4);
 
 			// Voyager-1 properties:
-			//System.out.println(earth.versor().magnitude());
+			final double voyagerDistance = 1500000.0;
+			final double voyagerMass = 751.0;
+			final Vector voyager = earth.versor().multiplyBy(earth.magnitude() + Re + voyagerDistance);
 
-			final Vector voyager = earth.versor().multiplyBy(earth.magnitude() + Re + 1500000.0);
 			final Vector Vt = voyager.versor().tangent().multiplyBy(-1.0);
 			final Vector Vn = Vt.tangent().multiplyBy(-1.0);
-			final Vector Vv = Vt.multiplyBy(11000.0).add(Ve.over(Vt)).subtract(Ve.over(Vn));
+			//final Vector Vv = Vt.multiplyBy(17000.0 + Ve.over(Vt).magnitude()) //17702.8
+			//		.add(Ve.over(Vn).multiplyBy(-1.0));
+			final Vector Vv = Ve.add(Vt.subtract(Vn)
+					.multiplyBy(11000.0));
 
-			/*System.out.println("Orthogonal?: " + Vn.dot(Vt));
-			System.out.println("voyager = " + voyager.getX() + " -> " + voyager.getY());
-			System.out.println("Ve  = " + Ve.getX() + " -> " + Ve.getY());
-			System.out.println("Vt = " + Vt.getX() + " -> " + Vt.getY());
-			System.out.println("Vn = " + Vn.getX() + " -> " + Vn.getY());
-			System.out.println("Ve over Vt = " + Ve.over(Vt).getX() + " -> " + Ve.over(Vt).getY());
-			System.out.println("Ve over Vn = " + Ve.over(Vn).getX() + " -> " + Ve.over(Vn).getY());
-			System.out.println("Vv  = " + Vv.getX() + " -> " + Vv.getY());*/
-
-			final double voyagerMass = 751.0;
-
-			// Bodies:
+			// Bodies (at "1977-Sep-05 12:56"):
 			final double [][] bodies = {
 				{voyager.getX(), voyager.getY(), 1.0, Vv.getX(), Vv.getY(), voyagerMass},
 				{0.0, 0.0, 6.963E+8, 0.0, 0.0, 1.988544E+30},
 				//{5.347050011076978E+10, -1.749871636534829E+10, 2440E+3, 5.681537103747067E+3, 4.849978521587917E+4, 3.302E+23},
 				//{2.169979322974804E+10, 1.055552194021935E+11, 6051.8E+3, -3.442327831494310+4, 6.878211961227505, 4.8685E+24},
-				{earth.getX(), earth.getY(), Re, Ve.getX(), Ve.getY(), 5.97219E+24},
+				{earth.getX(), earth.getY(), Re, Ve.getX(), Ve.getY(), earthMass},
 				//{1.332381415231438E+11, 1.767318038780731E+11, 3389E+3, -1.842109512919714E+4, 1.664469813414635E+4, 6.4185E+23},
-				{1.058409319749973E+11, 7.551533616543298E+11, 71492E+3, -1.310553498751214E+4, 2.424702557330317E+3, 1898.13E+24},
-				{-1.075592980165280E+12, 8.544801191238763E+11, 60268E+3, -6.541480412587013E+3, -7.590568168145934E+3, 5.68319E+26}
+				{1.052306803282038E+11, 7.552660211105334E+11, 71492E+3, -1.310809144834065E+4, 2.414193454159162E+3, 1898.13E+24},
+				{-1.075897485382291E+12, 8.541266557455370E+11, 60268E+3, -6.538637588011204E+3, -7.592554100817277E+3, 5.68319E+26}
 			};
 
 			// Para escalar el radio en Ovito:
 			final double [] scaleFactor = {
-				1500.0,		// Voyager-1
+				1.0,		// Voyager-1
 				1.0,		// Sun
 				//800.0,	// Mercury
 				//550.0,	// Venus
